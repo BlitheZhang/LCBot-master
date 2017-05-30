5c0e93f494d980d7a0ea3ec31195dd5f9abf4b9e
blithe <blitheshuo@gmail.com>
2017-05-30 12:52:22 +0800

    wechatbot

---
+++ export_puid.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from wxpy import *
+
+'''
+使用 cache 来缓存登陆信息，同时使用控制台登陆
+'''
+bot = Bot('bot.pkl', console_qr=False)
+
+
+'''
+开启 PUID 用于后续的控制
+'''
+bot.enable_puid('wxpy_puid.pkl')
+
+friends = bot.friends()
+groups = bot.groups()
+
+with  open('data', 'w',encoding='UTF-8') as output:
+    output.write("-----Groups-------\n")
+    for i in groups:
+        output.write(i.name + " ---> " + i.puid + "\n")
+
+# 启用 puid 属性，并指定 puid 所需的映射数据保存/载入路径
+
+# 指定一个好友
+my_friend = bot.friends().search('Blithe.@QingCloud')[0]
+
+# 查看他的 puid
+print(my_friend.puid)
+# 'edfe8468'
+
+### '''=   output.write("-----Friends-------\n")
+###    for i in friends:
+###        output.write(i.nick_name + " ---> " + i.puid + "\n")
+### '''   
+
